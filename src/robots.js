acd7ddc6edf5f4a159efb1761b4445ed5d3bfb6a
kareemkaddah <97706749+kareemkaddah@users.noreply.github.com>
2023-04-01 01:24:43 +0200

    adding all the files

---
+++ src/robots.js
@@ -0,0 +1,60 @@
+export const robots=[
+    {
+        id:0,
+        name:'kareem kaddah',
+        username:'Kareem',
+        email:'kareem.kaddah@gmail.com'
+
+    },
+    {
+        id:1,
+        name:'Assem kaddah',
+        username:'Assem',
+        email:'Assem.kaddah@gmail.com'
+
+    },
+    {
+        id:2,
+        name:'Dina kaddah',
+        username:'Dina',
+        email:'Dina.kaddah@gmail.com'
+
+    },
+    {
+        id:3,
+        name:'Diana kaddah',
+        username:'Diana',
+        email:'Diana.kaddah@gmail.com'
+
+    },
+    {
+        id:4,
+        name:'Majd kaddah',
+        username:'Majd',
+        email:'Majd.kaddah@gmail.com'
+
+    },
+    {
+        id:5,
+        name:'Mira kaddah',
+        username:'Mira',
+        email:'Mira.kaddah@gmail.com'
+
+    },
+    {
+        id:6,
+        name:'Tala Al shihabi',
+        username:'Tala',
+        email:'Tala.AlShihabi@gmail.com'
+
+    },
+    {
+        id:7,
+        name:'Ammar',
+        username:'Ammar',
+        email:'Ammar@gmail.com'
+
+    },
+    
+    
+]
